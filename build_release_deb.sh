397a2120d9bb7637af3b3e1ab4c971c894fd1884
Gracjan Jankowski <gracjan@man.poznan.pl>
2016-08-17 13:48:35 +0200

    fixes in debian packaging scripts

--- build_release_deb.sh
+++ build_release_deb.sh
@@ -22,21 +22,26 @@ mvn clean install
 # clone, configure (to use cdmi-s3-qos) and package CDMI server
 #
 git clone https://github.com/indigo-dc/CDMI.git
+cd CDMI
+git checkout 326eec3
+cd ..
 cp -rf config CDMI/
 rm -f CDMI/config/objectstore.properties
 cd CDMI
-git checkout 326eec3
+
 sed -i 's/dummy_filesystem/radosgw/g' config/application.yml
 sed -i 's/<dependencies>/<dependencies>\r\n<dependency>\r\n<groupId>pl.psnc<\/groupId>\r\n<artifactId>cdmi-s3-qos<\/artifactId>\r\n<version>0.0.1-SNAPSHOT<\/version>\r\n<\/dependency>/g' pom.xml
 mvn clean package -Dmaven.test.skip=true
 
 cp target/cdmi-server-0.1-SNAPSHOT.jar target/$NAME-${VERSION}-SNAPSHOT.jar
 
+
 cd ..
 
 mkdir -p debian/var/lib/$NAME/config/
 cp -r config/fixed-mode debian/var/lib/$NAME/config/
 cp CDMI/target/$NAME-${VERSION}-SNAPSHOT.jar debian/var/lib/$NAME/
+cp CDMI/config/* debian/var/lib/$NAME/config/
 
 chmod 0775 debian/DEBIAN/postinst
 
